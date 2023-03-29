e475b46af08112fea9dfea6d0fed187078e6390c
Mahmoud Salman <salmanmahmoud70@gmail.com>
2023-03-29 23:31:13 +0200

    create router folder and install tailwind

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="home bg-black w-full">
+        <h1 class="bg-slate-500  text-orange-950">
+            About
+        </h1>
+        <p class="text-[red]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit voluptatibus vero veniam quasi quod minima, enim, saepe corporis nihil, facilis officia alias itaque quas ut eius necessitatibus eveniet quisquam repellendus.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"about-view"
+    }
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="home bg-black font-bold">
+        Home
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"home-view"
+    }
+</script>
